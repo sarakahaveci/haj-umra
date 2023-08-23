6a3b5f41262002185dcb96e3279c5dbe0be60e65
sarakahveci <yamooonz@example.com>
2023-08-24 01:23:47 +0300

    fixed styling homepage

--- src/components/homepage/GalleryHomePage.js
+++ src/components/homepage/GalleryHomePage.js
@@ -4,26 +4,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import './Gallery.css'; // Import the styles
 
-const Gallery = () => {
+const GalleryHome = () => {
   const galleryData = [
     {
       id: 'package1',
-      image: 'https://images.unsplash.com/photo-1589827577276-65d717348780?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+      image: 'https://i.ibb.co/vjp2W02/gallery1.jpg',
       title: 'Haj Tours',
     },
     {
       id: 'package2',
-      image: 'https://images.unsplash.com/photo-1589827577276-65d717348780?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+      image: 'https://i.ibb.co/KFVrCV0/gallery9.jpg',
       title: 'Umra Tours',
     },
     {
       id: 'package3',
-      image:'https://images.unsplash.com/photo-1589827577276-65d717348780?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+      image:'https://i.ibb.co/9cTfmQW/gallery7.jpg',
       title: 'Culture Tours',
     },
     {
       id: 'package3',
-      image:'https://images.unsplash.com/photo-1589827577276-65d717348780?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+      image:'https://i.ibb.co/TB6rj6r/gallery8.jpg',
       title: 'Culture Tours',
     },
   ];
@@ -33,8 +33,8 @@ const Gallery = () => {
       <h2>Photo Gallery</h2>
       <div className="gallery-grid">
         {galleryData.map((item) => (
-          <Link key={item.id} to={`/gallery/${item.id}`} className="gallery-link">
-            <div className="gallery-column">
+          <Link key={item.id} to={`/gallery/`} className="gallery-link">
+            <div className="gallery-column" style={{height:"200px", maxWidth: "245px"}}>
               <img src={item.image} alt={`Photo ${item.id}`} />
             </div>
           </Link>
@@ -44,4 +44,4 @@ const Gallery = () => {
   );
 };
 
-export default Gallery;
+export default GalleryHome;
--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -8,18 +8,42 @@ import GalleryHomePage from "./GalleryHomePage";
 const Home = () => {
   return (
     <div>
-    <PhotoCarousel/>
-  <Tours/>
-  <div className="promo-text">
-  <h3>SİZİN İÇİN EN İYİSİNİ DÜŞÜNÜYORUZ</h3>
-  <p>
-    En kaliteli hizmeti en uygun fiyatlara sunuyoruz. Memnuniyetinizin en üst seviyede olacağını garanti ediyoruz.
-    Bu kutsal yolculuğunuzda Ammar Turizm olarak her konuda yardımcınız ve destekçiniz oluyoruz.
-  </p>
-</div>
-<HajUmra/>
-<GalleryHomePage/>
+      <PhotoCarousel/>
+      <Tours/>
+      <div className="promo-text">
+        <h3>SİZİN İÇİN EN İYİSİNİ DÜŞÜNÜYORUZ</h3>
+        <p>
+          En kaliteli hizmeti en uygun fiyatlara sunuyoruz. Memnuniyetinizin en üst seviyede olacağını garanti ediyoruz.
+          Bu kutsal yolculuğunuzda Ammar Turizm olarak her konuda yardımcınız ve destekçiniz oluyoruz.
+        </p>
       </div>
+      <HajUmra/>
+      <GalleryHomePage/>
+      <div className="video-container">
+        <div className="youtube-video">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/N9QSL1PLUS4"
+            title="Live Channel"
+            frameborder="0"
+            allow="autoplay; encrypted-media"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div className="youtube-video">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/EB0Y1ztWnic"
+            title="Live Channel"
+            frameborder="0"
+            allow="autoplay; encrypted-media"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
   );
 };
 
