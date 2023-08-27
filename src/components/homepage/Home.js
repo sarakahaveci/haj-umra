c6e5b3cd191315a9323864f6e90583c19dd88d45
sarakahveci <yamooonz@example.com>
2023-08-27 23:35:13 +0300

    fixed styling in lives and contacts

--- src/components/homepage/Home.js
+++ src/components/homepage/Home.js
@@ -14,35 +14,35 @@ const Home = () => {
         <h3>SİZİN İÇİN EN İYİSİNİ DÜŞÜNÜYORUZ</h3>
         <p>
           En kaliteli hizmeti en uygun fiyatlara sunuyoruz. Memnuniyetinizin en üst seviyede olacağını garanti ediyoruz.
-          Bu kutsal yolculuğunuzda Ammar Turizm olarak her konuda yardımcınız ve destekçiniz oluyoruz.
+          Bu kutsal yolculuğunuzda Alharamain Turizm olarak her konuda yardımcınız ve destekçiniz oluyoruz.
         </p>
       </div>
       <HajUmra/>
-      <GalleryHomePage/>
+      <GalleryHomePage />
       <div className="video-container">
-        <div className="youtube-video">
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/N9QSL1PLUS4"
-            title="Live Channel"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-          ></iframe>
-        </div>
-        <div className="youtube-video">
-          <iframe
-            width="560"
-            height="315"
-            src="https://www.youtube.com/embed/EB0Y1ztWnic"
-            title="Live Channel"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-          ></iframe>
-        </div>
+      <div className="youtube-video">
+        <iframe
+          width="100%" // Adjusted width to fit the container
+          height="315"
+          src="https://www.youtube.com/embed/N9QSL1PLUS4"
+          title="Live Channel"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        ></iframe>
       </div>
+      <div className="youtube-video">
+        <iframe
+          width="100%" // Adjusted width to fit the container
+          height="315"
+          src="https://www.youtube.com/embed/EB0Y1ztWnic"
+          title="Live Channel"
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>    
     </div>
   );
 };
--- src/components/services/Umra.js
+++ src/components/services/Umra.js
@@ -6,10 +6,90 @@ import './Services.css'; // Import the shared styles
 const Umra = () => {
   return (
     <div className="services-page">
-      <h2 className="services-title">Umra</h2>
-      <p className="services-description">Welcome to our Umra services page. Experience the spiritual journey of Umra with our dedicated and well-organized Umra packages.</p>
-      {/* Add more content here */}
-      <div className='div' style={{
+    <div className="services-page">
+  <h2 className="services-title">Umra</h2>
+  <p className="umra-meal-info">Sabah kahvaltısı + akşam yemeği açık büfe</p>
+
+  <table className="umra-table">
+    <thead>
+      <tr>
+        <th>Yürülem Mesafesi</th>
+        <th>4'lu Oda</th>
+        <th>3'lu Oda</th>
+        <th>2'li Oda</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>10 günlük 3 gece Medine</td>
+        <td>$14250</td>
+        <td>$1475</td>
+        <td>$1500</td>
+      </tr>
+      <tr>
+        <td>14 günlük 3 gece Medine</td>
+        <td>$1625</td>
+        <td>$1657</td>
+        <td>$1725</td>
+      </tr>
+      <tr>
+        <td>20 günlük 3 gece Medine</td>
+        <td>$2000</td>
+        <td>$2050</td>
+        <td>$2100</td>
+      </tr>
+    </tbody>
+  </table>
+<div className='sentence-container'>
+  <p className="umra-sentence mekke-sentence">Mekke Oteli: Süréyi 350 mt</p>
+  <p className="umra-sentence medine-sentence">Medine Oteli: Merkeziye 300-400 mt</p>
+  </div>
+
+  <table className="umra-table">
+    <thead>
+      <tr>
+        <th>Yürülem Mesafesi</th>
+        <th>4'lu Oda</th>
+        <th>3'lu Oda</th>
+        <th>2'li Oda</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>10 günlük 3 gece Medine</td>
+        <td>$14250</td>
+        <td>$1475</td>
+        <td>$1500</td>
+      </tr>
+      <tr>
+        <td>14 günlük 3 gece Medine</td>
+        <td>$1625</td>
+        <td>$1657</td>
+        <td>$1725</td>
+      </tr>
+      <tr>
+        <td>20 günlük 3 gece Medine</td>
+        <td>$2000</td>
+        <td>$2050</td>
+        <td>$2100</td>
+      </tr>
+      <tr>
+        <td>28 günlük 3 gece Medine</td>
+        <td>$1625</td>
+        <td>$1657</td>
+        <td>$1725</td>
+      </tr>
+    </tbody>
+  </table>
+  <div className="sentence-container">
+  <p className="mekke-sentence">MEKKE OTELI: SERVISLI OTELLERI 2000mt</p>
+  <p className="medine-sentence">Medine Oteli: Merkeziye 300-400mt</p>
+</div>
+</div>
+
+
+
+                  <div className='div' style={{
         height: '40vh'}}></div>
     </div>
   );
